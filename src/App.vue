42cfcdf9fcaaf2b76cfda2d537bb58b78fb6a9c3
Donato Riccio <donniepask@gmail.com>
2023-11-09 17:14:55 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script>
+import ComponenteA from './components/ComponenteA.vue';
+import ComponenteB from './components/ComponenteB.vue';
+
+import { store } from './store';
+
+import axios from 'axios';
+export default {
+    components: {
+        ComponenteA,
+        ComponenteB
+    },
+    data() {
+        return {
+            message: "CIAO",
+            store: store,
+        }
+    },
+    methods: {
+        decrease(value) {
+            // this.store.counter -= value;
+            console.log('decrease', value);
+        },
+        decreaseComplex(obj) {
+            // this.store.counter -= obj.value;
+            // console.log(obj.status);
+            console.log('decreaseComplex', obj);
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{ message }}</h2>
+    <ComponenteA />
+    <ComponenteB @decrease-counter="decrease" @decrease-counter-complex="decreaseComplex" />
+</template>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/ComponenteB.vue
@@ -0,0 +1,50 @@
+<script>
+import { store } from '../store.js'
+
+export default {
+    emits: ['decreaseCounter', 'decreaseCounterComplex'],
+    data() {
+        return {
+            store: store,
+
+        }
+    },
+    methods: {
+        add() {
+            // Modifico il valore di una proprietà dello store
+            this.store.counter++;
+        },
+        complexEmit() {
+            if (this.store.counter > 0) {
+                const obj = { value: 1, status: 'success' };
+                this.$emit('decreaseCounterComplex', obj)
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="buttons-container">
+        <!-- Chiamo un metodo del componente -->
+        <button @click="add()">Counter++ COMPONENTE</button>
+
+        <!-- Chiamo un metodo dello store -->
+        <button @click="store.storeAdd()">Counter++ STORE</button>
+
+        <button @click="$emit('decreaseCounter', 1)">Decrease Counter EMIT</button>
+
+        <button @click="complexEmit()">Decrease Counter Complex EMIT</button>
+    </div>
+</template>
+
+<style>
+.buttons-container {
+    display: flex;
+    flex-direction: column;
+}
+
+.buttons-container button {
+    margin: 10px 0;
+}
+</style>
